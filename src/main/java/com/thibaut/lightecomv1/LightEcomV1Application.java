0e5d9a09946aaff02b742f6f60594eeb6cd4563a
Thibaut G <thibaut.gobert@hotmail.com>
2019-05-31 14:21:09 +0200

    add dao interfaces

--- src/main/java/com/thibaut/lightecomv1/LightEcomV1Application.java
+++ src/main/java/com/thibaut/lightecomv1/LightEcomV1Application.java
@@ -1,13 +1,27 @@
 package com.thibaut.lightecomv1;
 
+import com.thibaut.lightecomv1.dao.CategoryRepository;
+import com.thibaut.lightecomv1.dao.ProductRepository;
+import com.thibaut.lightecomv1.entities.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LightEcomV1Application {
+public class LightEcomV1Application implements CommandLineRunner {
 
+    @Autowired
+    private ProductRepository productRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
     public static void main(String[] args) {
+
         SpringApplication.run(LightEcomV1Application.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Category ordinateurs = categoryRepository.save(new Category(null, "Ordinateurs", null, null));
+    }
 }
